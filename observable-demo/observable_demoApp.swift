d840fcd1b7cefd08d6b84a64f9c1990eb0af02f4
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-09 08:11:07 -0700

    Initial Commit

---
+++ observable-demo/observable_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  observable_demoApp.swift
+//  observable-demo
+//
+//  Created by user on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct observable_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
